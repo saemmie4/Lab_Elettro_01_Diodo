4dd9a0fa5d6ac9be0c02894fe6be1abece8e0b94
saemmie4 <samuele.bellini@gmail.com>
2025-11-21 17:05:23 +0100

    nah

--- fit_diodato.C
+++ fit_diodato.C
@@ -3,37 +3,44 @@
 #include "TF1.h"
 #include "TStyle.h"
 #include "TLegend.h"
+#include "TMath.h"
 
 void fit_diodato()
 {
-    TGraphErrors* graph_Si = new TGraphErrors("I_V_Si.txt", "%lg%lg%lg%lg");
+    TCanvas * canva = new TCanvas ("canva", "Fit canva", 0.,0., 800, 600);
+    canva->Divide(2, 1);
+    canva->cd(1);
+
+    TGraphErrors* graph_Si = new TGraphErrors("I_V_Si.txt", "%lg%lg%*lg%lg");
     TGraphErrors* graph_Ge = new TGraphErrors("I_V_Ge.txt", "%lg%lg%lg%lg");
 
     // TF1 * fit_Si = new TF1("fit_Si", "[0] + [1]*x", 0, 800);
     // TF1 * fit_Ge = new TF1("fit_Ge", "[0] + [1]*x", 0, 800);
     
 
-    TF1 * fit_Si = new TF1("fit_Si", "[0]*(TMath::Exp(x/[1]) - 1)", 0, 800);
-    TF1 * fit_Ge = new TF1("fit_Ge", "[0]*(TMath::Exp(x/[1]) - 1)", 0, 800);
+    // TF1 * fit_Si = new TF1("fit_Si", "[0]*(exp(x/[1]) - 1)", 0, 500);
+    TF1 * fit_Si = new TF1("fit_Si", "[0]*(TMath::Log(x/[1] + 1))", 0.05, 1);
+    TF1 * fit_Ge = new TF1("fit_Ge", "[0]*(exp(x/[1]) - 1)", 0, 400);
+    
+    fit_Si->SetParameter(1, 1e-6);
+    fit_Si->SetParameter(0,550);
+    // fit_Ge->SetParameter(0, 1);
+    // fit_Ge->SetParameter(1,200);
+
+    fit_Si->SetParName(0,"I0");
+    fit_Si->SetParName(1,"eta*VT");
+    fit_Ge->SetParName(0,"I0");
+    fit_Ge->SetParName(1,"eta*VT");
     
 
-    fit_Si->SetParName(0,"a");
-    fit_Si->SetParName(1,"b");
-    fit_Ge->SetParName(0,"a");
-    fit_Ge->SetParName(1,"b");
-    
-
-    graph_Si->Fit(fit_Si);
-    graph_Ge->Fit(fit_Ge);
+    graph_Si->Fit(fit_Si, "R");
+    // graph_Ge->Fit(fit_Ge, "R");
     
 
     graph_Si->SetTitle("I-V Silicio");
     graph_Ge->SetTitle("I-V Germanio");
 
     
-    TCanvas * canva = new TCanvas ("canva", "Fit canva", 0.,0., 800, 600);
-    canva->Divide(2, 1);
-    canva->cd(1);
     // gPad->SetLogy();
     graph_Si->Draw("APE");
     canva->cd(2);
